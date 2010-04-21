049009daafe1b96ba724b0688bfc9e3ce81f62d4
Alexey Vladykin <vladykin@users.noreply.github.com>
2010-04-22 03:45:21 +0400

    Changelog reimplemented on top of RecordSet

--- Pb/src/org/saturnine/api/Changeset.java
+++ Pb/src/org/saturnine/api/Changeset.java
@@ -16,18 +16,16 @@ public final class Changeset {
     private final String author;
     private final String comment;
     private final Date timestamp;
-    private final Changes changes;
 
     public Changeset(
             String id, String primaryParent, String secondaryParent,
-            String author, String comment, Date timestamp, Changes changes) {
+            String author, String comment, Date timestamp) {
         this.id = id;
         this.primaryParent = primaryParent;
         this.secondaryParent = secondaryParent;
         this.author = author;
         this.comment = comment;
         this.timestamp = timestamp;
-        this.changes = changes;
     }
 
     public String id() {
@@ -54,12 +52,34 @@ public final class Changeset {
         return timestamp;
     }
 
-    public Changes changes() {
-        return changes;
-    }
-
     @Override
     public String toString() {
         return id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Changeset)) {
+            return false;
+        }
+        final Changeset that = (Changeset) obj;
+        return this.id.equals(that.id)
+                && this.primaryParent.equals(that.primaryParent)
+                && (this.secondaryParent == null ? that.secondaryParent == null : this.secondaryParent.equals(that.secondaryParent))
+                && this.author.equals(that.author)
+                && this.comment.equals(that.comment)
+                && this.timestamp.equals(that.timestamp);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + this.id.hashCode();
+        hash = 53 * hash + this.primaryParent.hashCode();
+        hash = 53 * hash + (this.secondaryParent != null ? this.secondaryParent.hashCode() : 0);
+        hash = 53 * hash + this.author.hashCode();
+        hash = 53 * hash + this.comment.hashCode();
+        hash = 53 * hash + this.timestamp.hashCode();
+        return hash;
     }
 }
