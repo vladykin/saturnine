e2c73bfd29332ccbebb7365a325cf830179c447e
alexey_vladykin <alexey_vladykin@localhost>
2010-05-27 20:59:03 +0400

    status command: sort files

--- Pb/src/org/saturnine/cli/commands/StatusCommand.java
+++ Pb/src/org/saturnine/cli/commands/StatusCommand.java
@@ -2,6 +2,8 @@ package org.saturnine.cli.commands;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.TreeSet;
 import org.saturnine.api.PbException;
 import org.saturnine.cli.PbCommand;
 import org.saturnine.local.DirScanResult;
@@ -27,26 +29,30 @@ public class StatusCommand implements PbCommand {
         LocalRepository repository = LocalRepository.find(new File("."));
         try {
             DirScanResult workDirState = repository.getWorkDir().scan();
-            for (String path : workDirState.getAddedFiles().keySet()) {
+            for (String path : sort(workDirState.getAddedFiles().keySet())) {
                 System.out.println("A " + path);
             }
-            for (String path : workDirState.getRemovedFiles()) {
+            for (String path : sort(workDirState.getRemovedFiles())) {
                 System.out.println("R " + path);
             }
-            for (String path : workDirState.getMissingFiles()) {
+            for (String path : sort(workDirState.getMissingFiles())) {
                 System.out.println("! " + path);
             }
-            for (String path : workDirState.getUntrackedFiles()) {
+            for (String path : sort(workDirState.getUntrackedFiles())) {
                 System.out.println("? " + path);
             }
-            for (String path : workDirState.getModifiedFiles()) {
+            for (String path : sort(workDirState.getModifiedFiles())) {
                 System.out.println("M " + path);
             }
-            for (String path : workDirState.getUncertainFiles()) {
-                System.out.println("M " + path);
+            for (String path : sort(workDirState.getUncertainFiles())) {
+                System.out.println("?? " + path);
             }
         } catch (IOException ex) {
             throw new PbException("IOException", ex);
         }
+    }
+
+    private static Collection<String> sort(Collection<String> orig) {
+        return new TreeSet<String>(orig);
     }
 }
